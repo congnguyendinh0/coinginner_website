c064128162e63c323c8adada3617a3ff438dc5fa
congnguyendinh0 <congnguyendinh0@gmail.com>
2021-09-09 13:03:04 +0200

    message

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,10 @@ const IndexPage = () => (
     <Seo title="Home" />
     <h1>Hi people</h1>
     <p>Coinginner here.</p>
-    <p>Contact us: Go to contact us</p>
+    <p>Android App: https://play.google.com/store/apps/details?id=com.coinginner.coinginner&raii=com.coinginner.coinginner&rapt=AEjHL4N5zW6TWRfzJAHTi9njDfbXLFFKoz-EcK5ZfaV7S7XorEgNgs9x8SmFN1HmHGI6VqUzEinf0K7nXNFeFKR_aayCa6qdIWYtS7m9uiLtax3WAUIvdWI</p>
+    <Button variant="contained" color="primary">
+  Primary
+</Button>
     <p>
       <Link to="/page-2/">Contact US</Link> <br />
     </p>
