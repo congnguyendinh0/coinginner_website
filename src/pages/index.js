5eb1e8ad7edf844ff60dfb04a51d6f4bda066fad
congnguyendinh0 <congnguyendinh0@gmail.com>
2021-09-09 13:17:50 +0200

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,8 @@ const IndexPage = () => (
     <p><a href="https://play.google.com/store/apps/details?id=com.coinginner.coinginner&raii=com.coinginner.coinginner&rapt=AEjHL4N5zW6TWRfzJAHTi9njDfbXLFFKoz-EcK5ZfaV7S7XorEgNgs9x8SmFN1HmHGI6VqUzEinf0K7nXNFeFKR_aayCa6qdIWYtS7m9uiLtax3WAUIvdWI">Link To Playstore Android App</a></p>
     <p><a href="mailto:coinginner@gmail.com">Send Email To Test The iOS App on Testflight</a></p>
     <p><a href="https://github.com/congnguyendinh0/coinginner">Github Repo</a></p>
-    <Button variant="contained" color="primary">
-  Primary
-</Button>
+   
+
     <p>
       <Link to="/page-2/">Contact US</Link> <br />
     </p>
