b25d222f4c63276898c67c4ff032bcdff9f79f95
congnguyendinh0 <congnguyendinh0@gmail.com>
2021-09-09 13:13:05 +0200

    add link to android

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,9 @@ const IndexPage = () => (
     <Seo title="Home" />
     <h1>Hi people</h1>
     <p>Coinginner here.</p>
-    <p>Android App: https://play.google.com/store/apps/details?id=com.coinginner.coinginner&raii=com.coinginner.coinginner&rapt=AEjHL4N5zW6TWRfzJAHTi9njDfbXLFFKoz-EcK5ZfaV7S7XorEgNgs9x8SmFN1HmHGI6VqUzEinf0K7nXNFeFKR_aayCa6qdIWYtS7m9uiLtax3WAUIvdWI</p>
+    <p><a href="https://play.google.com/store/apps/details?id=com.coinginner.coinginner&raii=com.coinginner.coinginner&rapt=AEjHL4N5zW6TWRfzJAHTi9njDfbXLFFKoz-EcK5ZfaV7S7XorEgNgs9x8SmFN1HmHGI6VqUzEinf0K7nXNFeFKR_aayCa6qdIWYtS7m9uiLtax3WAUIvdWI">Link To Playstore Android App</a></p>
+    <p><a href="mailto:coinginner@gmail.com">Send Email To Test The iOS App on Testflight</a></p>
+    <p><a href="https://github.com/congnguyendinh0/coinginner">Github Repo</a></p>
     <Button variant="contained" color="primary">
   Primary
 </Button>
