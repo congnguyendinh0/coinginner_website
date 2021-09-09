cd806f13bbc634c8f62a24c0a6a1f76fd2fbf1d5
congnguyendinh0 <congnguyendinh0@gmail.com>
2021-09-09 12:01:14 +0200

    hello

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,16 +11,11 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Coinginner here.</p>
     <p>Contact us: Go to contact us</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
     <p>
       <Link to="/page-2/">Contact US</Link> <br />
+    </p>
+    <p>
+      <Link to="/page-3/">Privacy Policy</Link> <br />
     </p>
   </Layout>
 )
